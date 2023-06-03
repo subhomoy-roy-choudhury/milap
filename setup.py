1f1c8ea9cc952165de569c13102f4e1452022e7f
Subhomoy Roy Choudhury <64763636+subhomoy-roy-choudhury@users.noreply.github.com>
2023-06-03 20:05:07 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     long_description = "\n" + fh.read()
 
-VERSION = '0.0.3'
+VERSION = '0.0.4'
 DESCRIPTION = 'get meeting links of google meet according to your time slot and date.'
 LONG_DESCRIPTION = 'provide your oath2 credentials and other arguments as input and you are ready to get the meeting links.'
 
